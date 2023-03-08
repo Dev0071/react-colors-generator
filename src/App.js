0dc89dd96c57946611cf65ec9452b954ab1cb53b
Dev0071 <lecturergacheru71@gmail.com>
2023-03-08 19:58:18 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import SingleColor from './SingleColor'
+
+import Values from 'values.js'
+
+function App() {
+const [color, setColor] = useState('');
+const [error, setError] = useState (false);
+const [list, setList] = useState([]);
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  try {
+    let colors = new Values(color).all(10);
+    console.log(colors);
+  } catch (error) {
+    setError(true)
+    console.log(error);
+  }
+}
+
+  return <>
+    <section className='container'>
+      <h3>Color Generator</h3>
+      <form onSubmit={handleSubmit}>
+        <input type='text' placeholder='#f15025' value={color} onChange={(e) => setColor(e.target.value)} className={`${error? 'error' : null}`} />
+        <button type='submit'className='btn'>submit</button>
+      </form>
+    </section>
+    <section className='colors'>
+      <h4>list of colors</h4>
+    </section>
+  </>
+}
+
+export default App
