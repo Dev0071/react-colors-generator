477d65db99a65701a9621c99fa2409c5919dec32
Dev0071 <lecturergacheru71@gmail.com>
2023-03-08 22:13:19 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -12,10 +12,9 @@ const handleSubmit = (e) => {
   e.preventDefault();
   try {
     let colors = new Values(color).all(10);
-    console.log(colors);
+    setList(colors)
   } catch (error) {
     setError(true)
-    console.log(error);
   }
 }
 
@@ -29,6 +28,12 @@ const handleSubmit = (e) => {
     </section>
     <section className='colors'>
       <h4>list of colors</h4>
+      {
+        list.map((color, index) => {
+          console.log(color);
+          return <SingleColor key={index} {...color} index ={index} />
+        })
+      }
     </section>
   </>
 }
