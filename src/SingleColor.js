edcd9898576f17fbbb61cb9958188cb86b5dc61f
Dev0071 <lecturergacheru71@gmail.com>
2023-03-09 15:01:15 +0300

    set timeout function for copy to clipboard

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,25 +1,33 @@
-import React, { useState, useEffect } from "react";
-import rgbToHex from "./utils";
+import React, { useState, useEffect } from 'react';
+import rgbToHex from './utils';
 
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false);
-  const bcg = rgb.join(",");
+  const bcg = rgb.join(',');
   // console.log(bcg);
   // const hex = rgbToHex(...rgb);
   const hexValue = `#${hexColor}`;
 
+  // set timeout for copy to clipboard function
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setAlert(false);
+      return clearTimeout(timeOut);
+    }, 3000);
+  }, [alert]);
+
   return (
     <article
-      className={`color ${index > 10 && "color-light"}`}
+      className={`color ${index > 10 && 'color-light'}`}
       style={{ backgroundColor: `rgb(${bcg})` }}
       onClick={() => {
         setAlert(true);
         navigator.clipboard.writeText(hexValue);
       }}
     >
-      <p className="percent-value">{weight}%</p>
-      <p className="color-value">{hexValue}</p>
-      {alert && <p className="alert">copied to clipboard</p>}
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>{hexValue}</p>
+      {alert && <p className='alert'>copied to clipboard</p>}
     </article>
   );
 };
